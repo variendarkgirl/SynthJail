a06d55501af12d27c1b08cba0fd8b6314eecdf37
antimeme <124477449+variendarkgirl@users.noreply.github.com>
2025-02-18 22:19:10 +0500

    Create logger.py

---
+++ SynthJail_tool/logger.py
@@ -0,0 +1,19 @@
+import logging
+
+class Logger:
+    def __init__(self):
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S"
+        )
+        self.logger = logging.getLogger("SynthJail")
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
